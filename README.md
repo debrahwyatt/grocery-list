8a83b35c6f6a900f7663b56eaa0be6dd4fda9718
Debrah <debrahrwyatt@gmail.com>
2022-05-15 18:21:46 -0700

    White hole

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # grocery-list
 This is a program that converts Alexa's grocery list to a text document sorted by (Superstore) grocery isle.
+
+## Run the program
+
+Copy your Alexa list into shoppingList.txt
+
+Enter `./main.py` into the command line.
+
+Results are output as sortedShoppingList.txt
---
+++ main.py
@@ -0,0 +1,42 @@
+import re
+import json
+
+# Load in shoppingList.txt
+with open('shoppingList.txt') as f:
+  uList = f.readlines()
+
+# Parse and tokenize the list
+for i in range(len(uList)):
+  uList[i] = uList[i].replace("\n", "")
+  uList[i] = re.sub("^[\d]*. ", "", uList[i]).lower()
+
+# Create a blank list for sorted items
+sList = {}
+
+# Opening JSON file
+f = open('grocery.json')
+json_data = json.load(f)
+f.close()
+  
+# Check the list against grocery.json and sort it
+for x in json_data:
+  for item in json_data[x]:
+    if item.lower() in uList:
+      try:
+        sList[x].append(item)
+      except KeyError:
+        sList[x] = [item]
+  
+# TODO: if item not in list, add it to "UNKNOWN" key.
+
+# Save the list to file.
+f = open("sortedShoppingList.txt", "w")
+for x in sList:
+  f.write(x + "\n")
+  for item in sList[x]:
+    f.write(item + "\n")
+
+  if x != list(sList)[-1]:
+    f.write("\n\n")
+
+f.close()
